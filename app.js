eb5a259d8e4ebbeafd99afc2abdbe10be22f1670
Oriel Diamant <orieldiamant@gmail.com>
2020-12-06 03:25:21 +0200

    minor fix to the flipping pictures

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  function changePhoto() {
-    let pics = ["/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg"];
+    let pics = ["/images/4.jpg", "/images/2.jpg", "/images/3.jpg", "/images/1.jpg"];
     let image = document.getElementById("flip-image");
     let i = 0;
     setInterval(function () {

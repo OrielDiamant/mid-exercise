16a2565d2fc8f8997bc09927319e9014464f9e94
Oriel Diamant <orieldiamant@gmail.com>
2020-12-06 03:15:35 +0200

    replace the bottom middle picture every 5 seconds

---
+++ app.js
@@ -0,0 +1,11 @@
+ function changePhoto() {
+    let pics = ["/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg"];
+    let image = document.getElementById("flip-image");
+    let i = 0;
+    setInterval(function () {
+        if (i > 3) {
+            i = 0;
+        }
+        image.src = pics[i++];
+    }, 5000);
+}
